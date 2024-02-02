a0df0ba3cf9a8652f8529b343aef10a72436ca33
hbopche <143415625+hbopche@users.noreply.github.com>
2024-02-02 18:05:51 +0530

    Update HelloWorldServlet.java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/hello-world-maven")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
