f58000cc40c61ba9bf5e5fab3dc584f66c144391
hbopche <143415625+hbopche@users.noreply.github.com>
2024-02-02 17:47:27 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,29 @@
-import spark.servlet.SparkApplication;
+package com.springhow.example.helloworld;
 
-import static spark.Spark.get;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class HelloWorld implements SparkApplication {
-	public static void main(String[] args) {
-		new HelloWorld().init();
-	}
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
-	@Override
-	public void init() {
-		get("/hello", (req, res) -> "Hello World");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class);
+    }
+
+
+    @RequestMapping("/")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
 }
